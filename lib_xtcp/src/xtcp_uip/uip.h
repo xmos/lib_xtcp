e78f08330d3ee48d6045165ad7811d7e7959e5e9
Halley Kell <halley@custom-control.com>
2020-05-12 13:47:35 -0700

    Corrected uips handling of UDP multicast

--- lib_xtcp/src/xtcp_uip/uip.h
+++ lib_xtcp/src/xtcp_uip/uip.h
@@ -958,7 +958,7 @@ void uip_ipaddr_copy(void *dest, const void *src);
  */
 #if !UIP_CONF_IPV6
 int uip_ipaddr_cmp(const void *addr1, const void *addr2);
-#define uip_ipaddr_is_multicast(addr) ((((u16_t *)addr)[0] & 0xff) == 224)
+#define uip_ipaddr_is_multicast(addr) ((((u16_t *)addr)[0] & 0xf0) == 0xe0)
 #else /* !UIP_CONF_IPV6 */
 #define uip_ipaddr_cmp(addr1, addr2) (memcmp(addr1, addr2, sizeof(uip_ip6addr_t)) == 0)
 
