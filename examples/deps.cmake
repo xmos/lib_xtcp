e17fb108059ac89e22dd498396343c218374c6e3
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-05-07 17:22:14 +0100

    Cleaning up cmake files

--- examples/deps.cmake
+++ examples/deps.cmake
@@ -1,5 +1,3 @@
 set(APP_DEPENDENT_MODULES   lib_xtcp
-                            "lib_ethernet(4.0.1)"
-                            "lib_otpinfo(2.2.1)"
                             "lib_board_support(1.2.2)"
                             )
