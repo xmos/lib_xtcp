628f288585b03b4f166d8310826a13f2bd76baaa
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-05-15 10:14:28 +0100

    Builds both stacks with either cmake or makefile,
    Plus, Bumping dependency versions, adding optional headers, restoring original version number to cmake

--- lib_xtcp/lib_build_info.cmake
+++ lib_xtcp/lib_build_info.cmake
@@ -1,20 +1,22 @@
 set(LIB_NAME                lib_xtcp)
 
-set(LIB_VERSION             7.1.0) # Should this be 7 or 8? 7.0.0 is also used on Jakes branch
+set(LIB_VERSION             6.1.0)
 
 set(LIB_INCLUDES            api
                             src
                             src/xtcp_lwip/include
                             src/xtcp_lwip/xcore/include)
              
-set(LIB_DEPENDENT_MODULES   "lib_ethernet(4.0.0)"
+set(LIB_DEPENDENT_MODULES   "lib_ethernet(4.0.1)"
                             "lib_logging(3.3.1)"
                             "lib_xassert(4.3.1)"
-                            "lib_random(1.0.0)"
-                            "lib_otpinfo(2.0.0)")
+                            "lib_random(1.2.0)"
+                            "lib_otpinfo(2.2.1)")
 
 set(LIB_COMPILER_FLAGS      -g
                             -O3
                             -mno-dual-issue)
+
+set(LIB_OPTIONAL_HEADERS    xtcp_client_conf.h xtcp_conf.h)
 
 XMOS_REGISTER_MODULE()
