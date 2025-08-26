1348afb16180c04941c2183b05b9a3f4fa94fd8d
Humphrey Bucknell <humphreybucknell@xmos.com>
2025-08-26 16:00:00 +0100

    cmake updates

--- examples/deps.cmake
+++ examples/deps.cmake
@@ -1,3 +1,3 @@
 set(APP_DEPENDENT_MODULES   lib_xtcp
-                            "lib_board_support(1.2.2)"
+                            "lib_board_support(develop)" # Planning for 1.4.0
                             )
